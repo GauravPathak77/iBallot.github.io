381e3526e10b4c08d0334cce80613342c67e62a2
GauravPathak77 <115170827+GauravPathak77@users.noreply.github.com>
2023-04-08 10:07:01 +0530

    iBallot V1.2

--- event.js
+++ event.js
@@ -199,8 +199,8 @@ exports.Cal_event = function (startDate, endDate) {
       minute = `0${minute}`;
     }
 
-    // let currDate = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
-    let currDate2 = `${year}-${month}-${day}T${hour}:${minute}`;
+    let currDate2 = `${year}-${month}-${day}T${hour}:${minute}:00.000${TIMEOFFSET}`;
+    // let currDate2 = `${year}-${month}-${day}T${hour}:${minute}`;
     // let current_event = new Date(Date.parse(currDate));
     return currDate2.toString();
   };
